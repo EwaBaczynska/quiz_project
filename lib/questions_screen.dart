1bc6174fff1a798f67bcf28c22eef96103145380
EwaBaczynska <baczynska.ewa@gmail.com>
2024-03-12 22:13:39 +0100

    reusable answer_button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_project/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,33 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'THE QUESTIONS',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: ('Answer 1'),
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: ('Answer 2'),
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: ('Answer 3'),
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: ('Answer 4'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
