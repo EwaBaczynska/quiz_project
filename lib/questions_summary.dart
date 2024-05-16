300634f0835efae74b4a4ffeb794795c1510ec9f
EwaBaczynska <baczynska.ewa@gmail.com>
2024-05-16 12:40:41 +0200

    improving summary results and add restart function

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:quiz_project/questions_summary/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -7,22 +8,17 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map(
-        (data) {
-          return Row(children: [
-            Text(((data['question_index'] as int) + 1).toString()),
-            Column(
-              children: [
-                Text(data['question'] as String),
-                const SizedBox(height: 5),
-                Text(data['user_answer'] as String),
-                Text(data['correct_answer'] as String),
-              ],
-            ),
-          ]);
-        },
-      ).toList(),
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(data);
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
